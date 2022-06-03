e9ea409dca5cad859755f32442a220521841d761
Hector Garnica <106023753+Hec-Thor117@users.noreply.github.com>
2022-06-03 12:56:59 -0500

    Added updated files to merge to main

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const menuIcon = document.getElementsByClassName("container");
-const navBar = document.getElementsByClassName("navigation");
+const menuIcon = document.getElementsByClassName("menu-icon")[0];
+const navBar = document.getElementsByClassName("navigation")[0];
 
-menuIcon[0].addEventListener("click", () => {
-	menuIcon[0].classList.toggle("active");
-	navBar[0].classList.toggle("open");
+const fakeEmail = document.getElementsByClassName("fake-email")[0];
+const modal = document.getElementsByClassName("modal")[0];
+const closeBtn = document.getElementsByClassName("close")[0];
+
+
+menuIcon.addEventListener("click", () => {
+	menuIcon.classList.toggle("active");
+	navBar.classList.toggle("open");
+});
+
+fakeEmail.addEventListener("click", () => {
+	modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+	modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+	if (event.target === modal) {
+		modal.style.display = "none";
+	}
 });
